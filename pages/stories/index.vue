77875ebfaf7b9721b1700991228304e0865f37c6
RuslanPro01 <ruslan91999@gmail.com>
2020-05-03 20:59:24 +0300

    Структура проета V2.0

--- pages/stories/index.vue
+++ pages/stories/index.vue
@@ -1,29 +1,10 @@
 <template>
   <div>
-    <Header></Header>
-    <div class="breadcrumbs">
-      <a href></a>
-      <a href></a>
-    </div>
-    <search></search>
-    <motto></motto>
-    <section class="stories-people"></section>
-    <Header></Header>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import search from '@/components/ui/search';
-import Motto from '@/components/Motto';
 export default {
-  components: {
-    'main-header': Header,
-    'main-footer': Footer,
-    search,
-    'motto' : Motto
-  }
 };
 </script>
 
