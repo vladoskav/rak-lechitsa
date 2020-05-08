055d8eeb6ae824427d79151a4a6f378f3672a7fb
Atichka <atichka@MacBook-Air-Ekaterina.local>
2020-05-08 15:09:10 +0300

    add title

---
+++ components/Statistics.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="statistics">
+    <st-title class="statistics__title">Статистика по онкозаболеваниям</st-title>
+  </div>
+</template>
+
+<script>
+import Title from '@/components/ui/Title';
+  export default {
+    components: {
+      'st-title' : Title,
+    },
+  };
+</script>
+
+<style scoped>
+.statistics__title {
+  margin: 100px auto 0 60px;
+}
+</style>
---
+++ components/ui/Title.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1
+    :class="['title', `title_theme_${theme}`]"
+    >
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+  export default {
+    props: ['theme'],
+  };
+</script>
+
+<style scoped>
+.title {
+  max-width: 25%;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 2rem;
+  line-height: 2.25rem;
+
+  color: #000000;
+}
+
+.title_theme_line {
+  text-decoration-line: underline;
+}
+</style>
