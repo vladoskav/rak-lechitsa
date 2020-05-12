e61226869585e0b282a13bba15f38f4d65bdb945
user.email <neelkim1989@gmail.com>
2020-05-12 12:48:38 +0500

    feature: Добавил верстку кнопки "Оставить контакт"

--- components/ui/button.vue
+++ components/ui/button.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-
-  </div>
+  <button class="button">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 
 <style scoped>
+.button {
+  border: 0;
+  color: #fff;
+  background-color: rgb(97, 58, 147, 1);
+  margin-top: 56px;
+  width: 100%;
+  max-width: 280px;
+  min-height: 52px;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.1875rem;
+  font-style: normal;
+  text-align: center;
+  cursor: pointer;
+}
 
+.button:hover {
+  background-color: rgb(97, 58, 147, 0.9);
+}
 </style>
