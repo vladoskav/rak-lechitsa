55f2bacc8e3eeff3a96d1d4731faf37950cb0f4d
vladoskav <vladoskavkniaz@yandex.ru>
2020-05-15 15:13:22 +0300

    fix: исправлены замечания по ревью

--- components/LinkInstagram.vue
+++ components/LinkInstagram.vue
@@ -1,37 +1,44 @@
 <template>
   <section class="instagram">
-    <div class="text-block">
-      <a href="https://www.instagram.com/raklechitsa" class="text-block__link"
-        ><st-title :theme="'line'" class="text-block__title"
-          >Инстаграм</st-title
-        ></a
+    <container class="container">
+      <st-title :theme="'line'" class="text-block__title">
+        <a
+          href="https://www.instagram.com/raklechitsa"
+          target="_blank"
+          class="text-block__link"
+        >
+          Инстаграм</a
+        ></st-title
       >
+
       <p class="text-block__paragraph">
         Два раза в неделю мы просматриваем все посты по хештегу #этонелечится.
         Все истории, где нет нецензурных выражений и запрещенного контента
         попадают сюда. Следите за правильным написанием хештега, чтобы мы не
         пропустили вашу историю.
       </p>
-    </div>
-    <div class="grid">
-      <insta-element
-        class="grid__element"
-        v-for="image in images"
-        :bgImage="image.url"
-        :link="image.link"
-        :key="image.id"
-      />
-    </div>
+      <div class="grid">
+        <insta-element
+          class="grid__element"
+          v-for="image in images"
+          :bgImage="image.url"
+          :link="image.link"
+          :key="image.id"
+        />
+      </div>
+    </container>
   </section>
 </template>
 
 <script>
 import InstaElement from './ui/InstaElement';
 import Title from './ui/Title';
+import Container from './Container';
 export default {
   components: {
     'insta-element': InstaElement,
     'st-title': Title,
+    container: Container,
   },
   data() {
     return {
@@ -91,11 +98,17 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-height: 50vh;
+}
 .text-block__title {
-  width: 100%;
+  margin-bottom: 32px;
 }
 .grid__element {
-  width: 14vw;
+  width: 13vw;
   max-width: 195px;
 }
 .grid {
@@ -104,22 +117,10 @@ export default {
   grid-gap: 40px;
 }
 .instagram {
-  padding: 100px 0;
-  margin: 0 auto;
-  width: 92%;
-  max-width: 1320px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  padding: 100px 60px 100px 60px;
 }
 .text-block__link {
-  margin-bottom: 32px;
-}
-.text-block {
-  display: flex;
-  flex-direction: column;
-  max-width: 340px;
-  margin-right: 110px;
+  color: #000;
 }
 
 .text-block__paragraph {
@@ -128,27 +129,32 @@ export default {
   font-weight: normal;
   font-size: 18px;
   line-height: 22px;
-  color: #666666;
+  max-width: 340px;
+  color: #666;
 }
 @media screen and (max-width: 1280px) {
   .instagram {
-    max-width: 1180px;
+    padding: 90px 50px 90px 50px;
+  }
+  .grid {
+    grid-gap: 27px;
   }
 
   .text-block__paragraph {
     font-size: 16px;
     line-height: 20px;
+    max-width: 305px;
   }
 }
 @media screen and (max-width: 1024px) {
-  .instagram {
-    width: 90%;
-    max-width: 924px;
+  .grid {
+    grid-gap: 20px;
+    grid-template-columns: repeat(4, 130px);
   }
-
   .text-block__paragraph {
     font-size: 13px;
     line-height: 16px;
+    max-width: 260px;
   }
 }
 @media screen and (max-width: 768px) {
