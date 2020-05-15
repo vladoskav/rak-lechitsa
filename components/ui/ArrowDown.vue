4f77cddbd4d1ac8a8aae8e6b1b9709c70e7382d6
vladoskav <vladoskavkniaz@yandex.ru>
2020-05-15 13:33:21 +0300

    fix: пофикшен cover и удалены ненужные файлы

--- components/ui/ArrowDown.vue
+++ components/ui/ArrowDown.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="arrow-down"></button>
+  <div class="arrow-down"></div>
 </template>
 
 <script>
@@ -8,14 +8,10 @@ export default {};
 
 <style scoped>
 .arrow-down {
-  cursor: pointer;
   width: 36px;
   height: 12px;
   border: 0px solid transparent;
   background-color: transparent;
   background-image: url("data:image/svg+xml,%3Csvg width='36' height='12' viewBox='0 0 38 15' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M37 1L19 13L1 1' stroke='white' stroke-width='2'/%3E%3C/svg%3E%0A");
-}
-.arrow-down:focus {
-  outline: none;
 }
 </style>
