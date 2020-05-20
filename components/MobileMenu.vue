b792e514e03b5885e40cedc5e87773fd2e719b9f
Babeev <babeev.nikita@gmail.com>
2020-05-20 17:55:29 +0700

    fix menu

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -17,21 +17,16 @@ export default {
     container: Container,
   },
   methods: {
-    showPopup() {
-      this.$store.commit('popup/togglePopup');
+    showPopup(popup) {
+      this.$store.commit('popup/togglePopup', popup);
     },
-    methods: {
-      showPopup(popup) {
-        this.$store.commit('popup/togglePopup', popup);
-      },
-    },
-  }
+  },
 };
 </script>
 
 <style scoped>
 .header__container {
-  padding: 18px 50px;
+  margin: 18px 50px;
 }
 .header__button {
   font-family: 'Inter';
@@ -48,5 +43,24 @@ export default {
 
 .header__button:focus {
   outline: 0;
+}
+@media screen and (max-width: 768px) {
+.header__button {
+  font-size: 16px;
+  line-height: 24px;
+}
+}
+@media screen and (max-width: 480px) {
+.header__container {
+  margin: 18px 15px;
+}
+.header__menu {
+  flex-direction: column;
+  align-items: flex-start;
+}
+.header__button {
+  font-size: 13px;
+  line-height: 16px;
+}
 }
 </style>
